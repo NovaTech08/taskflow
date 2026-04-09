078d5389da1e80720f2b96245e2bd2b73c0a8af8
DanaBlanco <cdad1.2018@gmail.com>
2026-04-09 15:16:09 -0300

    feat: conectar opción 2 del menú para mostrar lista de tareas

--- taskflow/src/TaskFlow/Program.cs
+++ taskflow/src/TaskFlow/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using TaskFlow.Services;
+
+var service = new TaskService();
+
+while (true)
+{
+    Console.WriteLine("\n=== TASKFLOW ===");
+    Console.WriteLine("1. Crear tarea");
+    Console.WriteLine("2. Listar tareas");
+    Console.WriteLine("3. Cambiar estado");
+    Console.WriteLine("4. Salir");
+    Console.Write("Elegí una opción: ");
+
+    var opcion = Console.ReadLine();
+
+    switch (opcion)
+    {
+        case "2":
+            var tareas = service.GetAllTasks();
+            if (tareas.Count == 0)
+            {
+                Console.WriteLine("No hay tareas cargadas.");
+            }
+            else
+            {
+                foreach (var t in tareas)
+                {
+                    Console.WriteLine($"ID: {t.Id} | {t.Title} | Estado: {t.Status} | Responsable: {t.Responsible}");
+                }
+            }
+            break;
+
+        case "4":
+            return;
+
+        default:
+            Console.WriteLine("Opción no disponible aún.");
+            break;
+    }
+}
