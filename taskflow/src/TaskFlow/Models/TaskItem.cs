cd43b6f8e85a026b0ca8e7861f0857401d884edd
juanmm332 <juan.maria.mino@mi.unc.edu.ar>
2026-04-09 18:23:00 -0300

    feat: definir modelo TaskItem con campos obligatorios

---
+++ taskflow/src/TaskFlow/Models/TaskItem.cs
@@ -0,0 +1,28 @@
+using System;
+
+namespace TaskFlow.Models
+{
+   public enum TaskStatus
+   {
+      Pendiente,
+      EnProgreso,
+      Completada
+   }
+
+   public class TaskItem
+   {
+      public int Id { get; set; }
+
+      public string Titulo { get; set; }
+
+      public string Descripcion { get; set; }
+
+      public string Responsable { get; set; }
+
+      public TaskStatus Estado { get; set; } = TaskStatus.Pendiente;
+       
+      public DateTime FechaDeCreacion { get; set; } = DateTime.Now;
+
+      public DateTime? FechaDeModificacion { get; set; }
+   }
+}
